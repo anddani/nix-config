6cf8e806be2ec0bfab94a504b8942d1c5809954e
anddani <andredanielsson93@gmail.com>
2026-07-26 13:18:07 -0700

    Allow macbook to access t480

--- hosts/t480/power.nix
+++ hosts/t480/power.nix
@@ -1,7 +1,11 @@
-{ ... }:
+{ username, ... }:
 {
   # Headless box — manage it over SSH
   services.openssh.enable = true;
+
+  users.users.${username}.openssh.authorizedKeys.keys = [
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINHjf2BK92352c/LiDzrGRupBPcUw/hpP4t4Q9fXK51w andredanielsson93@gmail.com"
+  ];
 
   # Runs 24/7 with the lid closed
   services.logind.settings.Login = {
