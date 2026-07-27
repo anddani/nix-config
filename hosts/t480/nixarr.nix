94d8d681432902f3fc021ccdbf9ab76c72c8f5b6
anddani <andredanielsson93@gmail.com>
2026-07-26 17:13:51 -0700

    Update vpn conf

--- hosts/t480/nixarr.nix
+++ hosts/t480/nixarr.nix
@@ -1,23 +1,7 @@
-{ lib, username, ... }:
+{ username, ... }:
 {
   # Write access to the library dirs (they're setgid `media`, mode 2775)
   users.users.${username}.extraGroups = [ "media" ];
-
-  # VPN-Confinement doesn't reliably mount the namespace's resolv.conf into
-  # confined services when /etc/resolv.conf is a NetworkManager-managed
-  # symlink, leaving them without DNS (providers fail with
-  # "Temporary failure in name resolution"). Mount it explicitly.
-  systemd.services =
-    lib.genAttrs
-      [
-        "radarr"
-        "sonarr"
-        "prowlarr"
-        "bazarr"
-      ]
-      (_: {
-        serviceConfig.BindReadOnlyPaths = [ "/etc/netns/wg/resolv.conf:/etc/resolv.conf" ];
-      });
 
   # External HDD with the DVD backups. On the T480, find the drive with
   # `lsblk -f` and put its UUID here. If the drive is NTFS/exFAT, reformat
@@ -57,22 +41,18 @@
     radarr = {
       enable = true;
       openFirewall = true; # web UI on :7878
-      vpn.enable = true;
     };
     sonarr = {
       enable = true;
       openFirewall = true; # web UI on :8989
-      vpn.enable = true;
     };
     prowlarr = {
       enable = true;
       openFirewall = true; # web UI on :9696
-      vpn.enable = true;
     };
     bazarr = {
       enable = true;
       openFirewall = true; # web UI on :6767
-      vpn.enable = true;
     };
 
     transmission = {
@@ -80,7 +60,6 @@
       openFirewall = true; # web UI on :9091
       vpn.enable = true;
       # Required when vpn.enable is set (the module rejects the null default).
-      # Mullvad has no port forwarding, so inbound peers can't reach it anyway.
       peerPort = 51413;
     };
   };
